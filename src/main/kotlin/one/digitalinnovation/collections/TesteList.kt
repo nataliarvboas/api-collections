60bf022a0f119a09399b24e21ed59d1c2d32b845
nataliarvboas <nataliarvboas@gmail.com>
2021-03-04 15:08:11 -0300

    Adding TesteColecoesMutaveis.kt

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -2,8 +2,8 @@ package one.digitalinnovation.collections
 
 fun main() {
     val joao = Funcionario("Joao", 2000.0, "CLT")
-    val pedro = Funcionario("Pedro", 1500.0, "CLT")
-    val maria = Funcionario("Maria", 4000.0, "PJ")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
 
