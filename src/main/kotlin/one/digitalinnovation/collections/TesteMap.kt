6650bd34d9aa5c7250526643a805da694196db90
nataliarvboas <nataliarvboas@gmail.com>
2021-03-04 14:45:05 -0300

    Adding TesteMap.kt

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteMap.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val pair: Pair<String, Double> = Pair("Joao", 1000.0)
+    val map1 = mapOf(pair)
+    map1.forEach { k, v -> println("Chave: $k, Valor: $v") }
+
+    println("-------------------")
+    val map2 = mapOf("Pedro" to 2500.0,
+                     "Maria" to 3000.0)
+    map2.forEach { k, v -> println("Chave: $k, Valor: $v") }
+}
